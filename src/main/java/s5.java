b3d36ecc4e1e42310485c200d45046a36681a32e
plutik <you@exm>
2023-04-28 21:07:51 +0300

    s4 - это долбаная программа по добавлению в файл в нужной нам сортировке s5 это обратное действие

--- src/main/java/s5.java
+++ src/main/java/s5.java
@@ -1,50 +1,49 @@
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+// import java.util.Comparator;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class s5 {
-    public static void main(String[] args) {
-
+public class s5
+{
+    public static void main(String[] args) throws IOException{
         ArrayList<String> family = new ArrayList<>();
         ArrayList<String> name = new ArrayList<>();
         ArrayList<String> soname = new ArrayList<>();
-        ArrayList<Integer> age = new ArrayList<>();
-        int esc = 0;
-        while (esc==0){
-            esc = 1;
-            System.out.println("Введите фамилию следующего игрока");
-            Scanner q = new Scanner(System.in);
-            String in = q.nextLine();
+        ArrayList<String> age = new ArrayList<>();
+        //String [] str = new String[4];
+
+        File file = new File("testfile");
+        Scanner scanner= new Scanner(file);
+        String in = " ";
+        while(scanner.hasNextLine()){
+
+           // System.out.println(scanner.nextLine());// так а почему из за этой строчки все идет по одному месту?
+            in = scanner.nextLine();
             System.out.println(in);
-            family.add(String.valueOf(q));
+            String[] str = in.split(" ");
 
-
-
-            System.out.println("если хотите ввести еще одного игорака введите что угодно, если вы закончили введите -1- ");
-            //q = new Scanner(System.in);
-            String in1 = q.nextLine();
-
-            String perevod = String.valueOf(in1);
-            // String perevod2 = "да";
-
-            while (perevod == "да" ){
-                System.out.println("если вы закончили напишите -да-");
-                esc = 0;
-                perevod = "да";
-
-            }
+                family.add(str[0] + " ");
+                name.add(str[1] + " ");
+                soname.add(str[2] + " ");
+                age.add(str[3] + " ");
 
         }
-        //System.out.println(age.get(0));
-        for (int i = 0; i < family.size(); i++) {//цикл вывода
-            System.out.print(family.get(i) + " "+name.get(i) + " "+soname.get(i) + " "+age.get(i) + " ");
-            System.out.println("asd");
+scanner.close();
+        for (int i = 0; i<name.size();i++) {
+       System.out.print(age.get(i));
         }
 
-
-
-
+        age.sort(Comparator.naturalOrder());
+        for (int i = 0; i<name.size();i++) {
+            System.out.println(age.get(i));
+        }
     }
+
+
 }
+
+
