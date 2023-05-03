8dca8949dda830d671f961ee67a43047c40767ba
plutik <you@exm>
2023-05-03 22:16:08 +0300

    ДЗ++Создать множество, ключь и значение строки. Добавить шесть элементов. Вывести в консоль значения. Добавить ко всем значениям символ "!".     Создать второе множество с таким же обобщением. Ключи второго множества частично совпадают с ключеми первого. Объеденить значания во втором множестве и первом если ключи совподают. Вывести результат в консоль.
        программа s6

--- src/main/java/s6.java
+++ src/main/java/s6.java
@@ -1,6 +1,33 @@
+import java.util.HashMap;
+
 public class s6
 {
     public static void main(String[] args) {
-        System.out.println("1");
+        HashMap<String, String> map = new HashMap();
+        map.put("1","ОДИН");
+        map.put("2","ДВА");
+        map.put("3","ТРИ");
+        map.put("4","ЧЕТЫРЕ");
+        map.put("5","ПЯТЬ");
+        map.put("6","ШЕСТЬ");
+        map.forEach ((k,v)-> System.out.println(k+ " "+v));
+for (String key: map.keySet()) {
+    map.compute(key, (k,v)->v+"!");
+}
+        System.out.println();
+        map.forEach ((k,v)-> System.out.println(k+ " "+v));
+        HashMap<String, String> map1 = new HashMap();
+        map1.put("7","семь");
+        map1.put("8","восемь");
+        map1.put("3","ТРИ_c другого множества");
+
+        HashMap<String, String> map2 = new HashMap();
+        System.out.println();
+        map2.putAll(map);
+        System.out.println();
+        map2.forEach ((k,v)-> System.out.println(k+ " "+v));
+        System.out.println();
+        map2.putAll(map1);
+        map2.forEach ((k,v)-> System.out.println(k+ " "+v));
     }
 }
